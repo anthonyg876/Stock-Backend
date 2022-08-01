4de618b1dfddfd83c2ac1a3ec88e3484fd44ae77
anthonyg876 <anthonygravier2@gmail.com>
2022-07-31 21:00:49 -0400

    Updated project to load up the stocks and take them in

--- src/main/java/com/example/stockbackend/stocks/Stock.java
+++ src/main/java/com/example/stockbackend/stocks/Stock.java
@@ -1,6 +1,6 @@
 package com.example.stockbackend.stocks;
 
-import java.util.List;
+import java.util.ArrayList;
 
 import lombok.Setter;
 import lombok.Getter;
@@ -11,15 +11,13 @@ public class Stock {
     @Getter @Setter private double high = 0.0;
     @Getter @Setter private double low = 0.0;
 
-    @Getter @Setter private List<Double> stockHighs;
-    @Getter @Setter private List<Double> stockLows;
+    @Getter @Setter private ArrayList<Double> stockHighs;
+    @Getter @Setter private ArrayList<Double> stockLows;
 
-    public Stock(String name, double high, double low) {
-
+    public Stock(String name, ArrayList<Double> highs, ArrayList<Double> lows) {
+        this.name = name;
+        this.stockHighs = highs;
+        this.stockLows = lows;
     }
 
-    
-
-
-    
 }
--- src/main/java/com/example/stockbackend/stocks/StockController.java
+++ src/main/java/com/example/stockbackend/stocks/StockController.java
@@ -1,5 +1,24 @@
 package com.example.stockbackend.stocks;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "stocks")
 public class StockController {
     
+    @Autowired
+    private StockService stockService;
+
+    @GetMapping(value="/{stockName}")
+    public ResponseEntity<?> getStock(@PathVariable String stockName) {
+
+        return ResponseEntity.status(HttpStatus.OK).body(stockService.getStock(stockName));
+    }
+
 }
